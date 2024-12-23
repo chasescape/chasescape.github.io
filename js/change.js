84297f303aacf677c60eceea233c6dd9c0948b39
chasescape <143126761+chasescape@users.noreply.github.com>
2024-12-23 13:16:12 +0800

    12.23

--- js/change.js
+++ js/change.js
@@ -1,18 +1,18 @@
-// 获取切换表单的链接
-const toggleFormLinks = document.querySelectorAll('.toggle-form');
+const toggleForm = document.getElementById("toggle-form");
+const toggleFormRegister = document.getElementById("toggle-form-register");
 
-// 获取登录和注册表单容器
-const loginForm = document.querySelector('.login-form');
-const registerForm = document.querySelector('.register-form');
+// 获取注册和登录表单
+const registerForm = document.getElementById("register-form");
+const loginForm = document.getElementById("login-form");
 
 // 切换到注册表单
-toggleFormLinks[0].addEventListener('click', () => {
-    loginForm.style.display = 'none';
-    registerForm.style.display = 'block';
+toggleForm.addEventListener("click", function () {
+    loginForm.style.display = "none";
+    registerForm.style.display = "block";
 });
 
 // 切换到登录表单
-toggleFormLinks[1].addEventListener('click', () => {
-    registerForm.style.display = 'none';
-    loginForm.style.display = 'block';
+toggleFormRegister.addEventListener("click", function () {
+    registerForm.style.display = "none";
+    loginForm.style.display = "block";
 });
